9e8fc0931fa1f3b3ff58de557598de00f0f9a5d2
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-10-25 14:21:52 +0100

    Working on the monorepo architecture

---
+++ packages/scss/src/scripts/build.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const path = require('path');
+
+const sass = require('sass');
+
+const getComponents = () => {
+  let allComponents = [];
+  const types = ['atoms', 'molecules', 'organisms'];
+
+  types.map((t) => {
+    const allFiles = fs.readdirSync(`src/${t}`).map((file) => ({
+      input: `src/${t}/${file}`,
+      output: `src/lib/${file.slice(0, -4)}css`,
+    }));
+
+    allComponents.push(...allFiles);
+  });
+
+  return allComponents;
+};
+
+// compile scss file to css file
+const compile = (pathName, fileName) => {
+  const result = sass.renderSync({
+    data: fs.readFileSync(pathName).toString(),
+    outputStyle: 'expanded',
+    outFile: 'global.css',
+    includePath: [path.resolve('src')],
+  });
+
+  fs.writeFileSync(fileName, result.css.toString());
+};
+
+compile('src/global.scss', 'src/lib/global.css');
+
+getComponents().map((c) => {
+  compile(c.input, c.output);
+});
